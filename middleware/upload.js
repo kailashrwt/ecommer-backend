bde962a0a5643509de8377af37051d9f0df90273
kailashrwt <krwt2277@gmail.com>
2025-12-14 17:22:14 +0530

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,22 +1,23 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 
-const uploadPath = path.join(process.cwd(), "backend", "uploads");
+const uploadPath = path.join(process.cwd(), "uploads");
 
+// ensure folder exists (Render safe)
 if (!fs.existsSync(uploadPath)) {
-fs.mkdirSync(uploadPath, { recursive: true });
+  fs.mkdirSync(uploadPath, { recursive: true });
 }
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, uploadPath);
-    },
-    filename: function (req, file, cb){
-        const cleanName = file.originalname.replace(/\s+/g, "_");
-        cb(null, Date.now() + "_" + cleanName);
-    }
+  destination: (req, file, cb) => {
+    cb(null, uploadPath);
+  },
+  filename: (req, file, cb) => {
+    const cleanName = file.originalname.replace(/\s+/g, "_");
+    cb(null, Date.now() + "_" + cleanName);
+  },
 });
 
-module.exports = multer({storage});
-
+module.exports = multer({ storage });
 
