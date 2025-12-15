733462ad25762f42ba4b5a4b7ea6342420e74716
kailashrwt <krwt2277@gmail.com>
2025-12-15 20:45:42 +0530

    Update forgetPassword.js

--- routes/auth/forgetPassword.js
+++ routes/auth/forgetPassword.js
@@ -10,8 +10,8 @@ const router = express.Router();
    BREVO SMTP TRANSPORTER
 ========================= */
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
+  host: 'smtp-relay.brevo.com,
+  port: 587,
   secure: false,
   auth: {
     user: process.env.SMTP_USER,
